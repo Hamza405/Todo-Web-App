fb74247ec4deacc15e7f4a5175f9b99253d3ac83
Hamza405 <hamzash863@gmail.com>
2022-11-16 10:23:13 +0200

    Improve button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import style from "../styles/modules/button.module.scss";
+import { getStyleClasses } from "../utils/getStyleClasses";
+
+const buttonTypes = {
+  primary: "primary",
+  secondary: "secondary",
+};
 
 const Button = ({ children, variant = "primary" }) => {
   return (
-    <button className={style.button} type="button">
+    <button
+      className={getStyleClasses([
+        style.button,
+        style[`button--${buttonTypes[variant]}`],
+      ])}
+      type="button"
+    >
       {children}
     </button>
   );

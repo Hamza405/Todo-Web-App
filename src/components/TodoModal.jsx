7e3ab0c94c6290004b23f48584e299f32e22fc2b
Hamza405 <hamzash863@gmail.com>
2022-11-17 11:41:08 +0200

    handle form data

--- src/components/TodoModal.jsx
+++ src/components/TodoModal.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineClose } from "react-icons/md";
 import style from "../styles/modules/modal.module.scss";
 import Button from "./Button";
 
 const TodoModal = ({ handleCloseModal }) => {
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState("inComplete");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
         <div onClick={handleCloseModal} className={style.closeButton}>
           <MdOutlineClose />
         </div>
-        <form className={style.form}>
+        <form className={style.form} onSubmit={handleSubmit}>
           <h1 className={style.formTitle}>Add Task</h1>
           <label htmlFor="title">
             Title
-            <input type="text" id="title" />
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              id="title"
+            />
           </label>
           <label htmlFor="status">
             Status
-            <select id="status" name="status">
+            <select
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              id="status"
+              name="status"
+            >
               <option value="inComplete">In Complete</option>
               <option value="complete">Complete</option>
             </select>

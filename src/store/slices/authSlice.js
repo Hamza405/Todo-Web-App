c3935cc7ca22878e0390278a522a90f2129e630d
Hamza405 <hamzash863@gmail.com>
2022-11-20 11:09:19 +0200

    init auth slice

---
+++ src/store/slices/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initValue = {
+  user: {},
+  isAuth: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initValue,
+  reducers: {},
+});
+
+export default authSlice = authSlice.reducer;

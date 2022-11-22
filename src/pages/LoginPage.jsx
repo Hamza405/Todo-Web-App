0c3cdcf046416d2c868291a621744a45f364b78a
Hamza405 <hamzash863@gmail.com>
2022-11-22 20:53:30 +0200

    auth page state

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Button from "../components/Button";
 import style from "../styles/modules/register.module.scss";
 
 const LoginPage = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const inputData = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    if (inputData.email.trim() === "") {
+      setEmailError({ error: "Enter your email!" });
+      return;
+    }
+
+    if (inputData.password.trim() === "") {
+      setPasswordError({ error: "Enter your password!" });
+      return;
+    }
+    console.log(inputData);
+  };
   return (
     <div className={style.page__wrapper}>
       <div className={style.card}>
-        <form className={style.form}>
+        <form className={style.form} onSubmit={submit}>
           <h2 className={style.formTitle}>Login</h2>
           <label htmlFor="email">
             Email
-            <input type="email" id="email" />
+            <input ref={emailRef} type="email" id="email" />
+            {emailError && <p>Enter your Email</p>}
           </label>
-          <label htmlFor="password">
+          <label style={{ marginBottom: "25px" }} htmlFor="password">
             Password
-            <input id="password" type="password" />
+            <input ref={passwordRef} id="password" type="password" />
+            {passwordError && <p>Enter your password</p>}
           </label>
           <Button
             style={{ padding: "0.8rem 5rem" }}
@@ -24,7 +49,7 @@ const LoginPage = () => {
           >
             Login
           </Button>
-          <p>You don't have an account ? Register</p>
+          <p className={style.navigate}>You don't have an account ? Register</p>
         </form>
       </div>
     </div>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Button from "../components/Button";
 import style from "../styles/modules/register.module.scss";
 
 const RegisterPage = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const usernameRef = useRef();
+  const [emailError, setEmailError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [usernameError, setUsernameError] = useState(null);
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(usernameRef.current.value);
+    const inputData = {
+      username: usernameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    if (inputData.username.trim() === "") {
+      setUsernameError({ error: "Enter your username!" });
+      return;
+    }
+
+    if (inputData.email.trim() === "") {
+      setEmailError({ error: "Enter your email!" });
+      return;
+    }
+
+    if (inputData.password.trim() === "") {
+      setPasswordError({ error: "Enter your password!" });
+      return;
+    }
+    console.log(inputData);
+  };
   return (
     <div className={style.page__wrapper}>
       <div className={style.card}>
-        <form className={style.form}>
+        <form className={style.form} onSubmit={submit}>
           <h2 className={style.formTitle}>Register</h2>
           <label htmlFor="username">
             User Name
-            <input id="username" type="text" />
+            <input ref={usernameRef} id="username" type="text" />
+            {usernameError && <p>Enter your userName</p>}
           </label>
           <label htmlFor="email">
             Email
-            <input type="email" id="email" />
+            <input ref={emailRef} type="email" id="email" />
+            {emailError && <p>Enter your Email</p>}
           </label>
-          <label htmlFor="password">
+          <label style={{ marginBottom: "25px" }} htmlFor="password">
             Password
-            <input id="password" type="password" />
+            <input ref={passwordRef} id="password" type="password" />
+            {passwordError && <p>Enter your password</p>}
           </label>
           <Button
             style={{ padding: "0.8rem 5rem" }}
@@ -28,7 +62,7 @@ const RegisterPage = () => {
           >
             Sign up
           </Button>
-          <p> Do you have an account ? Login</p>
+          <p className={style.navigate}> Do you have an account ? Login</p>
         </form>
       </div>
     </div>

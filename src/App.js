82123d006d20501d71663ac772c029b1cb4673ec
Hamza405 <hamzash863@gmail.com>
2022-12-21 09:44:41 +0200

    Implement page lazy loadng

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import TodoPage from "./pages/TodoPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import { Toaster } from "react-hot-toast";
+
+// Components
+import Loading from "./components/Loading";
+
+// Pages
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const TodoPage = lazy(() => import("./pages/TodoPage"));
 
 function App() {
   const isAuth = useSelector((state) => state.auth.isAuth);
@@ -17,24 +23,26 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <TodoPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-        </Route>
-      </Routes>
-      <Toaster
-        toastOptions={{ className: "toaster" }}
-        position="bottom-center"
-      />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <TodoPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+          </Route>
+        </Routes>
+        <Toaster
+          toastOptions={{ className: "toaster" }}
+          position="bottom-center"
+        />
+      </Suspense>
     </BrowserRouter>
   );
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../styles/modules/app.module.scss";
+
+const Loading = () => {
+  return (
+    <div className={style.loading}>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
